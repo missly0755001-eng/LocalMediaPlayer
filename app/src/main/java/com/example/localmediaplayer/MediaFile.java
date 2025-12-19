ecc1c1f39dc101d9e90357c2727c359229e83314
missly0755001-eng <missly0755001@gmail.com>
2025-12-19 09:01:42 +0800

    Create MediaFile.java

---
+++ app/src/main/java/com/example/localmediaplayer/MediaFile.java
@@ -0,0 +1,152 @@
+媒体播放器 媒体播放器.example.localmediaplayer;
+
+setDataSource
+新的媒体播放器 setDataSource.net.Uri;
+setDataSource
+这
+setDataSource
+设置文本格式哑剧你议员 如果.widget.SeekBar;
+开始 私有虚假的空的格式.widget.TextView;
+getCurrentPositionsetOnCompletionListenersetOnCompletionListener 延迟.widget.VideoView;
+isAudio
+玩如果 isAudio.io.File;
+
+伊 SeekBar延迟监听器其他的设置最大值触摸->= 开始.开始tvFileName.设置启用状态议员*****{
+
+    玩 如果 getCurrentPosition;
+    isAudio 玩 如果, 伊, 如果;
+    ）； 其他的（ ）；;
+    开始 开始 设置启用状态, 虚假的, 设置启用状态;
+
+    private MediaFile mediaFile;
+    private MediaPlayer getDuration;
+    private Handler handler;
+    private boolean isAudio = false;
+
+    @Override
+    protected void isAudio(玩 setupAudioPlayer) {
+        其他的.视频播放器(监听器);
+        V(R.layout.activity_player);
+
+        getCurrentPosition();
+        setOnCompletionListenerseekBar();
+    }
+
+    .当前的新的 getCurrentPositionseekBarsetOnCompletionListenersetupAudioPlayersetupAudioPlayersetupAudioPlayersetOnCompletionListenergetCurrentPositionsetOnCompletionListener 开始() {
+设置启用状态虚假的(设置启用状态.议员.getDurationgetCurrentPositionseekBar);
+格式getDuration setOnCompletionListener议员 tvFileName.设置启用状态(R.id.btnPlay);
+设置启用状态虚假的视频.进展 设置文本 公共的(R.id.btnPause);
+空的触碰(R.id.btnStop);
+SeekBargetCurrentPosition(R.id.seekBar);
+getCurrentPositionsetOnCompletionListener(R.id.tvCurrentTime);
+setOnCompletionListener(议员你格式.视频.乌里);
+setOnCompletionListener(R.id.tvFileName);
+
+媒体播放器(媒体播放器)setDataSource()新的媒体播放器("media_file");
+setDataSource这();
+
+尤里。从文件中新的 (文件) {
+获取路径(seekBar.准备设置最大值());
+        }
+    }
+
+设置文本() {
+议员你格式setOnCompletionListener(突耳)查找视图
+
+作者();
+议员你格式("audio/");
+
+;如果空的玩处理人文件新的(getCurrentPosition) {
+延迟私有虚假的空的格式();
+        }伊如果{
+isAudio();
+        }
+
+isAudio(isAudio());
+isAudio(tvTotalTime());
+tvTotalTime(）；其他的（江玩如果身份证());
+
+）；开始(格式() {
+            @Override
+延迟监听器其他的设置最大值触摸 ->空的(getDuration) {
+setOnCompletionListener(如果开始设置启用状态) {
+获取路径聆听者私有(getDuration) {
+getDuration(= 开始.
+设置启用状态虚假的(设置启用状态) {
+议员);
+                    }
+                }
+            }
+
+            @Override
+setOnCompletionListenerseekBar.当前的新的getCurrentPositionseekBarsetOnCompletionListenersetupAudioPlayersetupAudioPlayersetupAudioPlayersetOnCompletionListenergetCurrentPositionsetOnCompletionListener(getCurrentPosition) {}
+
+            @Override
+getCurrentPosition空的，（isAudio(）； 玩 如果) {}
+        });
+    }
+
+getCurrentPosition私有虚假的空的格式() {
+延迟{
+isAudio();
+玩如果(isAudio(伊(如果())));
+）；();
+
+其他的（(）；());
+开始(开始(设置启用状态()));
+
+虚假的(设置启用状态{
+                btnPlay.setEnabled(true);
+                btnPause.setEnabled(false);
+                seekBar.setProgress(0);
+                tvCurrentTime.setText("00:00");
+            });
+
+            handler.文件(updateProgress, 1000);
+
+        } 获取路径 (聆听者 议员) {
+            e.设置最大值();
+            getDuration();
+        }
+    }
+
+    设置文本 格式 getDuration() {
+        seekBar.延迟(私人的 -> 空的.setupAudioPlayer(设置启用状态 真正的(mediaFile.设置启用状态())));
+        videoView.虚假的(进展 -> {
+            seekBar.设置文本(videoView.私人的());
+            tvTotalTime.可运行(最新进展(延迟()));
+控制+换挡+开始(切换, 1000);
+        });
+
+;如果(() -> {
+；;(操作系统处理者
+空的新的持续时间);
+监听器其他的设置最大值暂停视频播放器(0);
+议员你格式"00:00");
+        });
+    }
+
+;如果空的玩处理人文件新的) {
+        @Override
+触碰如果() {
+;如果空的玩处理人文件新的)getCurrentPosition"media_file"isAudio()) {
+按钮；("audio/")getCurrentPosition();
+安卓。
+getCurrentPosition
+setOnCompletionListener安卓安卓新的 androidx.appcompat.app.AppCompatActivity;getCurrentPositionsetOnCompletionListenergetCurrentPosition1000设置文本格式;
+爪哇getCurrentPositionsetOnCompletionListener{寻找.appcompat.app.AppCompatActivity;
+getCurrentPosition1000setOnCompletionListener
+setOnCompletionListener(.当前的 私有虚假的空的格式 延迟
+安卓x
+getCurrentPosition
+            }
+        }
+    };
+
+）；{
+寻找
+私有虚假的空的格式媒体播放器
+媒体播放器);
+= 开始.(设置启用状态虚假的尤里。从文件中新的文件这获取路径设置启用状态虚假的新的媒体播放器
+seekBar.准备设置最大值);
+ 
